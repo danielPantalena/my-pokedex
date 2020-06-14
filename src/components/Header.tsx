a278754d0f1c9f8e83aa15b04717a11a5dcd223a
Daniel Pantalena <pantalenadaniel@gmail.com>
2020-06-14 08:49:11 -0300

    Add routes and Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+
+const Header = () => {
+  return (
+    <React.Fragment>
+      <header>
+        <img src="" alt="pokemon" />
+        <nav>
+          <Button variant="secondary">Primary</Button>
+        </nav>
+      </header>
+    </React.Fragment>
+  );
+};
+
+export default Header;
---
+++ src/routes.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Pokedex from './components/Pokedex/Pokedex';
+import PokemonDetail from './Pages/PokemonDetail'
+
+const Routes = () => {
+  return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Pokedex} />
+          <Route path="/:name" component={PokemonDetail} />
+        </Switch>
+      </BrowserRouter>
+  );
+};
+
+export default Routes;
