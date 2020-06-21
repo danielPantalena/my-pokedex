75cdb3e4bb4405606328fb088bcd489628cb0fa3
Daniel Pantalena <pantalenadaniel@gmail.com>
2020-06-21 16:45:25 -0300

    Updates

---
+++ precache-manifest.093e3792779acd97c556ba27a8a91d83.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c51c6a67e160fdd565079a0cf528d72",
+    "url": "/index.html"
+  },
+  {
+    "revision": "431c1b9a1c7c81ffe4b0",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "3ad8051b639e94f8dfc2",
+    "url": "/static/css/main.b3be4d18.chunk.css"
+  },
+  {
+    "revision": "431c1b9a1c7c81ffe4b0",
+    "url": "/static/js/2.895d2e37.chunk.js"
+  },
+  {
+    "revision": "bdd9529717cef61cf6dac816210badc0",
+    "url": "/static/js/2.895d2e37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ad8051b639e94f8dfc2",
+    "url": "/static/js/main.1e852333.chunk.js"
+  },
+  {
+    "revision": "45a62228ec41ca58a1d8",
+    "url": "/static/js/runtime-main.8c898ed3.js"
+  }
+]);
